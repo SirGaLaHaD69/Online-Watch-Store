e6507e512d3a1b8dafcd421b636e0c00fb124543
Sir GaLaHaD <58564570+SirGaLaHaD69@users.noreply.github.com>
2021-01-19 19:55:11 +0530

    changed Procfile

--- ecom/settings.py
+++ ecom/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = MEDIA_DIR
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRs = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'build/static')
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
