4defad74339f1572b2d5cec4d709ff926713e64b
Sir GaLaHaD <58564570+SirGaLaHaD69@users.noreply.github.com>
2021-01-19 18:37:35 +0530

    final-commit-1

--- ecom/urls.py
+++ ecom/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf.urls.static import static
 from . import settings
-
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls',namespace = 'api')),
+    re_path('.*',TemplateView.as_view(template_name = 'index.html'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
