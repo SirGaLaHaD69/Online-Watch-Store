f4319c71af6ab047323e4d573e3b4118a5381193
Sir GaLaHaD <58564570+SirGaLaHaD69@users.noreply.github.com>
2021-01-20 13:56:52 +0530

    reinstalled s3

--- ecom/settings.py
+++ ecom/settings.py
@@ -169,6 +169,6 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 PUBLIC_MEDIA_LOCATION = 'media/images'
-MEDIA_URL = 'http://%s.s3.amazonaws.com/media/images/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/images/'
 # DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
