16c799a18737595027184e92df116f946a15d1e5
Sir GaLaHaD <58564570+SirGaLaHaD69@users.noreply.github.com>
2021-01-20 14:24:05 +0530

    reinstalled s3

--- ecom/settings.py
+++ ecom/settings.py
@@ -168,7 +168,7 @@ AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = 'ASIA347A4W5KR6GP2R4T'
 AWS_SECRET_ACCESS_KEY = 'qvSblRKjeZeP/qqSnSipfPvQ2FkCMAEfD3cvaL+Q'
 AWS_STORAGE_BUCKET_NAME ='marlin-watch-store'
-PUBLIC_MEDIA_LOCATION = 'media/images'
+PUBLIC_MEDIA_LOCATION = 'media/'
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/images/'
 # DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
